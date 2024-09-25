78870a31ef519f667451a12c486b6fe5dcf5b7e4
Seph-Anthony <generalgenx60@gmail.com>
2024-09-25 15:10:40 +0800

    goods na

--- src/sqlarambala2c/Main.java
+++ src/sqlarambala2c/Main.java
@@ -1,11 +1,10 @@
 
 package sqlarambala2c;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Scanner;
+import java.util.*;
+import java.sql.*;
+
+
 
 
 public class Main {
@@ -13,7 +12,7 @@ public class Main {
         Connection con = null;
         try {
             Class.forName("org.sqlite.JDBC"); // Load the SQLite JDBC driver
-            con = DriverManager.getConnection("jdbc:sqlite: test2.db"); // Establish connection
+            con = DriverManager.getConnection("jdbc:sqlite:test.db"); // Establish connection
             System.out.println("Connection Successful");
         } catch (Exception e) {
             System.out.println("Connection Failed: " + e);
@@ -23,29 +22,31 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-        System.out.println("Enter Student ID: ");
-        int id = in.nextInt();
-        in.nextLine();
-        System.out.println("Enter First Name: ");
-        String fname = in.nextLine();
-        System.out.println("Enter Last Name: ");
-        String lname =in.nextLine();
-        System.out.println("Enter Email: ");
-        String em = in.next();
-        System.out.println("Enter Status");
-        String sts = in.next();
         
-        String sql = "Insert Into Studentsss (id, fname, lname, email, status) VALUES (?, ?, ?, ?, ?)";
+        
+        System.out.println("Enter Student ID: ");
+        int s_id = in.nextInt();
+      
+        System.out.println("Enter First Name: ");
+        String f_name = in.next();
+        System.out.println("Enter Last Name: ");
+        String l_name =in.next();
+        System.out.println("Enter Email: ");
+        String e_m = in.next();
+        System.out.println("Enter Status");
+        String s_ts = in.next();
+        
+        String sql = "Insert Into Student (s_id, s_fname, s_lname, s_email, s_status) VALUES (?, ?, ?, ?, ?)";
         
         try {
             
                 Connection con = connectDB();
                 PreparedStatement pst = con.prepareStatement(sql);
-                pst.setInt(1, id);
-                pst.setString (2, fname);
-                pst.setString (3, lname);
-                pst.setString (4, em);
-                pst.setString (5, sts);
+                pst.setInt(1, s_id);
+                pst.setString (2, f_name);
+                pst.setString (3, l_name);
+                pst.setString (4, e_m);
+                pst.setString (5, s_ts);
                 pst.executeUpdate();
                 
                 System.out.println("Insert Successfully");
